a89a07b628e0807ec9b330f5a386cb3252ce288b
KalebParra007 <kalebparramarin135@gmail.com>
2024-09-16 12:14:28 -0500

    se crean modelos de datos

---
+++ src/main/java/com/example/SURATECH5/MODELOS/SignoVital.java
@@ -0,0 +1,9 @@
+package com.example.SURATECH5.MODELOS;
+
+public class SignoVital {
+
+    //nombre
+    //valor
+    //fechaTomo
+    //
+}
---
+++ src/main/java/com/example/SURATECH5/MODELOS/paciente.java
@@ -0,0 +1,25 @@
+package com.example.SURATECH5.MODELOS;
+
+public class paciente {
+
+    String nombres;
+
+    String cedula;
+
+    Integer EDAD;
+
+    Integer ips;
+
+    String RH
+
+
+
+
+
+
+
+
+            ()
+
+
+}
